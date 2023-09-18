77f371773f763903ae5c02a2ccaded75d444c8a9
sultanpeyek <sultanpeyek999@gmail.com>
2023-09-18 18:22:06 +0700

    lint fix

--- src/lib/extract-data.ts
+++ src/lib/extract-data.ts
@@ -8,9 +8,9 @@ export function extractData(data: DAS.GetAssetResponse[]): Asset[] {
       name: item.content?.metadata.name ?? '',
       symbol: item.content?.metadata.symbol ?? '',
       image: item.content?.links?.image ?? '',
-      collectionName: item.grouping?.find(
-        group => group.group_key === 'collection',
-      )?.group_value,
+      collectionName:
+        item.grouping?.find(group => group.group_key === 'collection')
+          ?.group_value ?? '',
     }
   })
 
